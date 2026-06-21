c7cd0b6809c7e947646ae114cd2f94acf010e602
Dilwar <ahmeddilwar45@gmail.com>
2026-06-21 11:01:39 +0530

    Version 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type":"text/html"});
-    res.end("Jenkins CI/CD Pipeline Version 2 🚀");
+    res.send("Hello from Jenkins CI/CD!");
 });
 
 server.listen(PORT, () => {
